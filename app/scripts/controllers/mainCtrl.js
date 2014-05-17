d54af67dae7d6dd28e9304427939370be9d69dc4
tommaso rossi <kurtommy@gmail.com>
2014-05-17 22:04:42 +0200

    no message

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -2,9 +2,20 @@
 
 angular.module('smartApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+    $scope.myInterval = 2000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+      var newWidth = 600 + slides.length;
+      console.log('http://placekitten.com/' + newWidth + '/300');
+      slides.push({
+        image: 'http://placekitten.com/' + newWidth + '/300',
+        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      });
+    };
+    for (var i=0; i<4; i++) {
+      $scope.addSlide();
+  }
+    
   });
